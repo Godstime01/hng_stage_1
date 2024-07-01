741daf016c0fdf9b28a4fd20217c27fe3d21906b
godstime01 <youngestkid01@gmail.com>
2024-07-01 19:31:35 +0100

    hello working

--- core/views.py
+++ core/views.py
@@ -4,19 +4,26 @@ from rest_framework.views import View
 from django.http import JsonResponse
 from django.conf import settings
 
-def get_location(ip):
+def get_location(ip="129.205.113.176"):
     url = f'https://geo.ipify.org/api/v2/country,city?apiKey={settings.GEO_IPY_KEY}&ipAddress={ip}'
     response = requests.get(url)
     data = response.json()
-    print(data)
+    print(data.get('location'))
+    l = data.get('location')
 
-    return data.get('location')["city"]
+    return l["city"]
 
 # Function to fetch temperature using OpenWeatherMap API
-def get_temperature(city="New york"):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
+def get_temperature(city):
+    print(city, "CITY")
+    if city:
+
+       url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
+    else:
+        url = f'http://api.openweathermap.org/data/2.5/weather?q=new york&appid={settings.OPENWEATHER_API_KEY}&units=metric'
     response = requests.get(url)
     data = response.json()
+    print(data)
     temperature = data['main']['temp']
     return temperature
 
@@ -26,6 +33,7 @@ class Index(View):
         user_ip = request.META.get('REMOTE_ADDR', '')
         
         location = get_location(user_ip)
+        print(location)
         temperature = get_temperature(location)
         
         res = {
